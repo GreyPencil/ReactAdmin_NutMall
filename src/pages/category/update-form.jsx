6d2aae49624020bf89444a68ba87ec6cfaadbd15
suze_glb <kangsuze@gmail.com>
2021-02-26 21:45:42 -0500

    Some updates

--- src/pages/category/update-form.jsx
+++ src/pages/category/update-form.jsx
@@ -1,58 +1,4 @@
 import React, {Component} from 'react'
-<<<<<<< HEAD
-import {
-    Form,
-    Select,
-    Input
-} from 'antd'
-// import { Form} from '@ant-design/compatible';
-//
-// import '@ant-design/compatible/assets/index.css';
-
-const Item = Form.Item
-const Option = Select.Option
-
-
-class AddForm extends Component {
-
-
-    render() {
-        // const {getFieldDecorator} = this.props.form
-        return (
-            <Form>
-                <Item name='parentId' initialValue={'0'}>
-                    {/*{*/}
-                    {/*    getFieldDecorator('parentId', {*/}
-                    {/*        initialValue: '0'*/}
-                    {/*    })(*/}
-                            <Select>
-                                <Option value='0'>一级分类</Option>
-                                <Option value='1'>book</Option>
-                                <Option value='2'>clothes</Option>
-
-                            </Select>
-                        {/*)*/}
-                    {/*}*/}
-
-                </Item>
-
-                <Item name='categoryName' initialValue={''}>
-                    {/*{*/}
-                    {/*    getFieldDecorator('categoryName', {*/}
-                    {/*        initialValue: ''*/}
-                    {/*    })(*/}
-                            <Input placeholder='Please input the category name'/>
-                        {/*)*/}
-                    {/*}*/}
-                </Item>
-            </Form>
-        )
-    }
-}
-
-// export default Form.create(AddForm)
-export default (AddForm)
-=======
 import PropTypes from 'prop-types'
 import {
   Form,
@@ -101,4 +47,3 @@ class UpdateForm extends Component {
 }
 
 export default Form.create()(UpdateForm)
->>>>>>> 60afdd4 (authorization)
